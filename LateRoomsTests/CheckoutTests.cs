afdd2ed1a98cacbec29c5ce8d9e48629dedbb0e2
Alan Broadhead <alan.broadhead@gmail.com>
2019-02-28 12:43:22 +0000

    added test for checks where there is more that the 3 items but still get discount for the 3 and pay full price for the other 1

--- LateRoomsTests/CheckoutTests.cs
+++ LateRoomsTests/CheckoutTests.cs
@@ -56,8 +56,26 @@ namespace LateRoomsTests
             var totalPrice = checkout.GetTotalPrice();
 
             Assert.AreEqual(130, totalPrice);
+        }
+
+        [TestMethod]
+        public void Adding4ItemsOfSameTypeAGivesDiscountPricePlus1FullPrice()
+        {
+            var itemA1 = new StockKeepItemA();
+            var itemA2 = new StockKeepItemA();
+            var itemA3 = new StockKeepItemA();
+            var itemA4 = new StockKeepItemA();
 
 
+            var checkout = new Checkout();
+            checkout.Scan(itemA1);
+            checkout.Scan(itemA2);
+            checkout.Scan(itemA3);
+            checkout.Scan(itemA4);
+
+            var totalPrice = checkout.GetTotalPrice();
+
+            Assert.AreEqual(180, totalPrice);
         }
 
     }
